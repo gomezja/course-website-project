d5cbb126dc5ad462f0eac13715d0bf715b9d9e23
gomezja <jgomez42gb@gmail.com>
2018-04-15 19:45:17 -0700

    added improved navbar and responsive flexbox

--- myScripts.js
+++ myScripts.js
@@ -1,10 +1,33 @@
-// gets called when user preses hamburger icon
-function responsiveManager() {
+// hamburger icon is pressed
+function hamburgerIconClicked() {
     var nav = document.getElementById("nav");
-
+    var ham = document.getElementById("ham");
+    
+    // display / hide responsive dropdown menu for smaller
+    // screens and transform hamburger icon to ham / X
     if (nav.className === "navbar") {
         nav.className += " responsive";
+        ham.className += " icon-x";
     } else {
         nav.className = "navbar";
+        ham.className = "nav-icon";
     }
+
+    return false;
+}
+
+// dropdown nav item is clicked
+function dropdownClicked() {
+    var drop = document.getElementById("dropdown");
+
+    // display dropdown items and rotate dropdown icon
+    if (drop.className === "dropdown-content") {
+        drop.className = "dropdown-content clicked";
+        document.getElementById("dropdown-icon").style.transform = "rotate(180deg)";
+    } else {
+        drop.className = "dropdown-content";
+        document.getElementById("dropdown-icon").style.transform = "none";
+    }
+
+    return false;
 }
